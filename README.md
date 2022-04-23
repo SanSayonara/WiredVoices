fc600ce9dc2c74a501f6be14e0b7907b3b9e7f47
= <sanbil@protonmail.com>
2022-04-23 00:20:41 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Wired Voices
+
+Wired Voices is an anonymous real-time ephemeral textboard inspired by the now-defunct imageboard [mebious.co.uk](http://mebious.co.uk/) and Serial Experiments Lain. It has been designed with simplicity and anarchy in mind.
+
+**STILL UNDER DEVELOPMENT**
---
+++ frontend/webpack.config.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'wiredVoices.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    })
+  ]
+};
+
+module.exports = config;
