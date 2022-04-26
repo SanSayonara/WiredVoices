610cc72b74e9f3ba605c092923e2b373c7c4c459
= <sanbil@protonmail.com>
2022-04-26 18:48:53 -0400

    Frontend: Added more design and message form structure

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,19 @@
-import './styles.scss';
+import './styles/animations.scss';
+import './styles/main.scss';
+import './styles/form.scss';
+
+let messagesToDeleteList = [];
+
+const viewportHeight = window.innerHeight;
+const viewportWidth = window.innerWidth;
+
+const instructionsDOM = document.querySelector('#instructions');
+const messageForm = document.querySelector('#messageForm');
+
+const instructionsFontSize = viewportWidth / instructionsDOM.innerHTML.length;
+
+instructionsDOM.style.fontSize = `${instructionsFontSize}px`;
+
+messageForm.onsubmit = event => {
+    event.preventDefault(), console.log(event)
+};
