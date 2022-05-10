ad39177498fee3e2d409297e23fc54a2f75c6373
= <sanbil@protonmail.com>
2022-05-09 23:10:38 -0400

    Frontend: Fixed bug on lain apparition

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -106,16 +106,18 @@ function connectToServer() {
   };
 }
 
-lainElement.onload = () => {
-  const { x, y } = getRandomCoordinates(lainElement);
-
-  lainElement.style.left = `${x}px`;
-  lainElement.style.top = `${y}px`;
-  lainElement.classList.add('showLainAnimation');
-};
-
 const showLain = () => {
   const randomImage = _.sample(lainImages);
+
+  if (lainElement.onload === null) {
+    lainElement.onload = () => {
+      const { x, y } = getRandomCoordinates(lainElement);
+
+      lainElement.style.left = `${x}px`;
+      lainElement.style.top = `${y}px`;
+      lainElement.classList.add('showLainAnimation');
+    };
+  }
 
   lainElement.src = randomImage;
 };
